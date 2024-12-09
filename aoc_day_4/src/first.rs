721d9b970f30c05b8751fca1eeec66da09895312
zvaktualno <tadej.zivic@hotmail.com>
2024-12-09 19:31:34 +0100

    solved day4

--- aoc_day_4/src/first.rs
+++ aoc_day_4/src/first.rs
@@ -1,3 +1,5 @@
+use std::array::TryFromSliceError;
+
 fn vector_compare(v1: &[u8], v2: &[u8]) -> bool{
     if v1.len() != v2.len(){
         return false
@@ -6,6 +8,7 @@ fn vector_compare(v1: &[u8], v2: &[u8]) -> bool{
 }
 
 fn find_horizontally(map: &Vec<Vec<u8>>) -> i32 {
+    // Horizontal search is done by moving a window over each line and scanning for the word and its reverse value.
     let mut result = 0;
     for line in map{
         let word1 = String::from("XMAS");
@@ -14,24 +17,130 @@ fn find_horizontally(map: &Vec<Vec<u8>>) -> i32 {
         let res2 = line.windows(word1.len()).filter(|sw| vector_compare(sw, word2.as_bytes())).count();
         result += res1 + res2;
     }
-    dbg!(result);
-    return 0;
+    return result as i32;
 }
-fn find_vertically(map: &Vec<Vec<u8>>) -> i32 {
-    let flipped_map = map.clone().reverse();
 
-    0
+fn find_vertically(map: &Vec<Vec<u8>>) -> i32 {
+    // Vertical search is done by rotating the 2D matrix and searching horizontally
+
+    let mut flipped_map: Vec<Vec<u8>> = vec![];
+    for cidx in 0..map[0].len(){
+        let mut new_line: Vec<u8> = vec![];
+        for lidx in 0..map.len() {
+            new_line.push(map[lidx][cidx]);
+        }
+        flipped_map.push(new_line);
+    }
+
+    return find_horizontally(&flipped_map);
+}
+
+fn find_diagonally_minus45(map: &Vec<Vec<u8>>) -> i32 {
+    // Vertical search is done by rotating the 2D matrix and searching horizontally
+
+    assert!(map.len() == map[0].len(), "The input matrix must be a square.");
+    let mut flipped_map: Vec<Vec<u8>> = vec![];
+
+
+    for i in 0..map.len(){
+        let mut new_line: Vec<u8> = vec![];
+
+        let mut line = 0;
+        let mut col: usize = map[0].len() - 1 - i;
+
+        while line < map[0].len() && col < map[0].len() {
+            new_line.push(map[line][col]);
+            line +=  1;
+            col +=  1;
+        }
+        flipped_map.push(new_line);
+    }
+
+    for i in 0..map.len(){
+        let mut new_line: Vec<u8> = vec![];
+
+        let mut line: usize = i + 1;
+        let mut col: usize = 0;
+
+        while line < map[0].len() && col < map[0].len() {
+            new_line.push(map[line][col]);
+            line +=  1;
+            col +=  1;
+        }
+        flipped_map.push(new_line);
+    }
+
+
+    return find_horizontally(&flipped_map);
+}
+
+fn find_diagonally_plus45(map: &Vec<Vec<u8>>) -> i32 {
+    // Vertical search is done by rotating the 2D matrix and searching horizontally
+
+    assert!(map.len() == map[0].len(), "The input matrix must be a square.");
+    let mut flipped_map: Vec<Vec<u8>> = vec![];
+
+
+    for i in 0..map.len(){
+        let mut new_line: Vec<u8> = vec![];
+
+        let mut line = 0;
+        let mut col: usize = i;
+
+        loop {
+            new_line.push(map[line][col]);
+            if col == 0 {
+                break;
+            }
+            line +=  1;
+            col -=  1;
+        }
+        flipped_map.push(new_line);
+    }
+
+    for i in 0..map.len()-1{
+        let mut new_line: Vec<u8> = vec![];
+
+        let mut line: usize = i + 1;
+        let mut col: usize = map.len() -1 ;
+
+
+        loop {
+            new_line.push(map[line][col]);
+            if line == map.len() - 1 {
+                break;
+            }
+            line +=  1;
+            col -=  1;
+        }
+        flipped_map.push(new_line);
+    }
+
+
+    return find_horizontally(&flipped_map);
 }
 
 pub fn solve(lines: &Vec<String>) {
     let mut res: i32 = 0;
     let mut charmap: Vec<Vec<u8>> = vec![];
+
     for line in lines{
         let l = line.as_bytes();
         let l = Vec::from(l);
         charmap.push(l);
     }
 
-    find_horizontally(&charmap);
+    res += find_horizontally(&charmap);
+    res += find_vertically(&charmap);
+
+    //let testmap: Vec<Vec<u8>> = vec![
+    //    vec![1,2,3],
+    //    vec![4,5,6],
+    //    vec![7,8,9],
+    //];
+    //dbg!(&testmap);
+    res += find_diagonally_minus45(&charmap);
+    res += find_diagonally_plus45(&charmap);
+        
     println!("Part one result {}", res);
 }
--- aoc_day_4/src/second.rs
+++ aoc_day_4/src/second.rs
@@ -1,4 +1,53 @@
+fn vector_compare(v1: &[u8], v2: &[u8]) -> bool{
+    if v1.len() != v2.len(){
+        return false
+    }
+    return v1.iter().zip(v2.iter()).filter(|&(a, b)| a == b).count() == v1.len();
+}
+
+fn is_mas(v1: &Vec<u8>) -> bool{
+    let mas = Vec::from("MAS");
+    let sam = Vec::from("SAM");
+
+    return vector_compare(v1, &mas) || vector_compare(v1, &sam);
+}
+
+fn create_cross(map: &Vec<Vec<u8>>, lidx: usize, cidx: usize) -> (Vec<u8>, Vec<u8>){
+
+    assert!(lidx > 0 && lidx < map.len() - 1 && cidx > 0 && cidx < map.len() - 1, "Wrong index");
+
+    let v1 = vec![map[lidx-1][cidx-1], map[lidx][cidx], map[lidx+1][cidx+1]];
+    let v2 = vec![map[lidx+1][cidx-1], map[lidx][cidx], map[lidx-1][cidx+1]];
+
+    return (v1, v2);
+}
+
+fn scan_map(map: &Vec<Vec<u8>>) -> i32 {
+    let mut res = 0;
+    for lidx in 1..map.len()-1{
+        for cidx in 1..map.len()-1{
+            if map[lidx][cidx] == b'A'{
+                let (v1, v2) = create_cross(map, lidx, cidx);
+                if is_mas(&v1) && is_mas(&v2) {
+                    res += 1;
+                }
+            }
+        }
+    }
+
+    return res;
+}
+
 pub fn solve(lines: &Vec<String>) {
     let mut res: i32 = 0;
+    let mut charmap: Vec<Vec<u8>> = vec![];
+
+    for line in lines{
+        let l = line.as_bytes();
+        let l = Vec::from(l);
+        charmap.push(l);
+    }
+
+    res = scan_map(&charmap);
     println!("Part two result {}", res);
 }
